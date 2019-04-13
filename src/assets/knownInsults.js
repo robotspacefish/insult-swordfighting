74e1732ce0117f19415c7a6bd78ab4abbe0c952c
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-04-12 20:44:45 -0400

    insults

---
+++ src/assets/knownInsults.js
@@ -0,0 +1,16 @@
+const knownInsults = {
+  insults: [
+  "You fight like a Dairy Farmer!",
+  "This is the END for you, you gutter crawling cur!",
+  "Boy are you ugly!",
+  "What an idiot!"
+  ],
+  comebacks: [
+    "How appropriate! You fight like a cow!",
+    "And I've got a little TIP for you, get the POINT?",
+    "Oh yeah?",
+    "I am rubber, you are glue."
+  ]
+}
+
+export default knownInsults;
