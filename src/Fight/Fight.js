755dd4e18474b1454d56e737014bc7503f86e10e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-10 09:01:51 -0400

    WIP Fight layout and functionality

--- src/Fight/Fight.js
+++ src/Fight/Fight.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Images from '../Images/Images';
+import Messages from '../Messages/Messages';
+import Choices from '../Choices/Choices';
+import Scroll from '../Scroll/Scroll';
 
-const Fight = ()=> {
-  const turn = () => {
-    let insults = [...knownInsults.insults];
-    let comebacks = [...knownInsults.comebacks]
+export default class Fight extends Component {
+  constructor(props) {
+    super(props);
+    this.updateTurn = this.updateTurn.bind(this);
+  }
 
-    displayPlayerInsultChoices(turnType === 'insults' ? insults : comebacks);
-  };
+  updateTurn(choice) {
+    // TODO
+    this.props.updateTurnType();
+    this.props.updateTurn();
+  }
 
-  const displayPlayerInsultChoices = (choices => choices.map(choice => ));
-};
+  render() {
+    return (
+      <div className="Fight container">
+        <Images />
+        <Messages
+           txt={this.props.txt}
+         />
+         <Scroll>
+          <Choices
+             choices={this.props.choices}
+             updateTurn={this.updateTurn}
+          />
+        </Scroll>
+      </div>
+    )
+  }
+}
 
-export default Fight;
+// const turn = () => {
+//   let insults = [...knownInsults.insults];
+//   let comebacks = [...knownInsults.comebacks]
+
+//   displayPlayerInsultChoices(turnType === 'insults' ? insults : comebacks);
+// };
+
+// const displayPlayerInsultChoices = (choices => choices.map(choice => ));
