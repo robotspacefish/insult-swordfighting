ff3c8bedb9cd9c91e484109903c060bde3a117b9
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-10 09:26:55 -0400

    add styles to Debug

--- src/Debug.js
+++ src/Debug.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './Debug.css';
 
 export default class Debug extends Component {
   render() {
     return (
-      <div className="debug">
-        <span style={{ paddingTop: '2px' }}>[ Debug ]</span>
+      <div className="Debug">
+        <p>[ Debug ]</p>
         <button onClick={() => this.props.updateMode('title')}>Title Mode</button>
         <button onClick={() => this.props.updateMode('turnStart')}>TurnStart Mode</button>
         <button onClick={() => this.props.updateMode('fight')}>Fight Mode</button>
