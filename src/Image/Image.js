7142c152f39655d8ee422c4bc68450e302c1d7a0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-10 09:27:58 -0400

    add placeholder alt tag to image

--- src/Image/Image.js
+++ src/Image/Image.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Image = ({ text }) => <img className="pirate" src={`https://via.placeholder.com/200x250.png?text=${text}`} />;
+const Image = ({ text }) => <img className="pirate" src={`https://via.placeholder.com/200x250.png?text=${text}`} alt="placeholder" />;
 
 export default Image;
