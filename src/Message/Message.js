20a6b33596684e5f4ab8a3eb61f72bf37cddb7cd
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-15 12:01:44 -0400

    add if msg exists to conditional

--- src/Message/Message.js
+++ src/Message/Message.js
@@ -3,7 +3,7 @@ import './Message.css';
 
 const Message = ({ name, msg }) => {
   // if (name.length > 0 && msg.length> 0) {
-  if (msg.length > 0) {
+  if (msg && msg.length > 0) {
     return <p id="message"><span id="speaker-name">{name}</span>: {msg}</p>
   } else {
     return null;
