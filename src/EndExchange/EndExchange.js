56d2f813bad8b0d5d5ce587357ff7a927cc67b80
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-16 12:41:00 -0400

    add a class to display the exchange winner in different colors

--- src/EndExchange/EndExchange.js
+++ src/EndExchange/EndExchange.js
@@ -3,8 +3,7 @@ import './EndExchange.css';
 
 export default class EndExchange extends Component {
   render() {
-    let turn = this.props.turnType === 'insult' ? 'player' : 'pirate';
-    // let loser = this.props.winner === 'player' ? 'pirate' : 'player';
+    const turn = this.props.turnType === 'insult' ? 'player' : 'pirate';
     let msg = '';
     if (this.props.winner === 'draw') {
       msg = `Draw! ${turn} turn.`
@@ -12,8 +11,9 @@ export default class EndExchange extends Component {
       msg = `${this.props.winner} wins the exchange. ${this.props.winner} goes again.`
     }
 
+    const playerWinClass = this.props.winner === 'player' ?`EndExchange-${this.props.winner}-win` : '';
     return (
-      <div className="EndExchange">
+      <div className={`EndExchange ${playerWinClass}`}>
         <p>{msg}</p>
       </div>
     )
