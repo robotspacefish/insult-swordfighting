156851fb9304551ce0a133781b4821bb1d7e0eec
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-16 12:42:36 -0400

    display round winner

--- src/FightEnd/FightEnd.js
+++ src/FightEnd/FightEnd.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react'
 import './FightEnd.css';
 
 export default class FightEnd extends Component {
-  static defaultProps = { modeToSet : 'roundStart'};
+  static defaultProps = { modeToSet : 'fightStart'};
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
+    this.props.initNextRound();
     this.props.updateMode(this.props.modeToSet);
     // TODO fadeout effect
   }
   render() {
     return (
       <div className="FightEnd container">
-        <p>This component displays who won the previous round before starting the next round. </p>
-        <button onClick={this.handleClick}>Continue</button>
+        <p>{this.props.winner} wins! </p>
+        <button className="btn" onClick={this.handleClick}>Continue</button>
       </div>
     )
   }
