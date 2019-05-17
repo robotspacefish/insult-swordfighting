0d3e4d576755717a7db5e30524b5797b79c26396
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-17 14:20:02 -0400

    add instructions

--- src/Instructions/Instructions.js
+++ src/Instructions/Instructions.js
@@ -6,7 +6,11 @@ const Instructions = () => {
     <div className="Instructions container">
       <div className="Instructions-content container">
         <h2>How to Play</h2>
-        <p>Etiam porta sem malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec sed odio dui.</p>
+        <p>Each round, you select an insult from the choices. Your "pirate opponent" will comeback and, if it's the correct comeback, it will be their turn to insult you. Then you select a comeback from your choices that matches their insult.</p>
+        <p>An insult that receives the incorrect comeback results in a point for the insulter. The first to 2 consecutive points wins the round.</p>
+        <p>There are 16 insults and comebacks to learn (not including the nonsense ones like "Boy are you ugly!" and "Oh yeah?").</p>
+        <p>Once you learn them all you win!</p>
+
       </div>
     </div>
   );
