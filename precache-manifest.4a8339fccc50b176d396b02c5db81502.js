3745eb641afee638cf3960559f4fcc1c4fbbb91b
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-17 14:25:10 -0400

    Updates

---
+++ precache-manifest.4a8339fccc50b176d396b02c5db81502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e049a9492dc00ebf332",
+    "url": "/insult-swordfighting/static/css/main.0a309ec0.chunk.css"
+  },
+  {
+    "revision": "1e049a9492dc00ebf332",
+    "url": "/insult-swordfighting/static/js/main.22bfa5a6.chunk.js"
+  },
+  {
+    "revision": "664f3908cf4c52ae6a52",
+    "url": "/insult-swordfighting/static/js/runtime~main.24aaa282.js"
+  },
+  {
+    "revision": "525f855f0b380ae86927",
+    "url": "/insult-swordfighting/static/js/2.5e02d234.chunk.js"
+  },
+  {
+    "revision": "12507d43eb040661785979e447f1be8b",
+    "url": "/insult-swordfighting/index.html"
+  }
+];
