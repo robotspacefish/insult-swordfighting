fb3e41a2da8962da8046bde3f111eddcf76df65d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2019-05-17 14:19:54 -0400

    add title content

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -20,8 +20,8 @@ export default class Title extends Component {
     return (
       <div className="Title container">
         <h1>Insult <span>Swordfighting</span></h1>
-        <h2>Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</h2>
-        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas faucibus mollis interdum.</p>
+        <h2>Based on the mechanic from The Secret of Monkey Island by Ron Gilbert</h2>
+        <p>How many rounds will it take you to become good enough to challenge the Sword Master?</p>
         <button className="Title-start-btn btn" onClick={this.handleClick}>Begin!</button>
       </div>
     )
